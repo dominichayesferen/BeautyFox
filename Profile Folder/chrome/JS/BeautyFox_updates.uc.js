8ec9d120dde7a287e3afcb6b54b890cec1167b32
dominichayesferen <ferenosdev@outlook.com>
2026-06-12 10:18:38 +0100

    A better NCP.

--- Profile Folder/chrome/JS/BeautyFox_updates.uc.js
+++ Profile Folder/chrome/JS/BeautyFox_updates.uc.js
@@ -5,13 +5,26 @@
 // ==/UserScript==
 
 function bfCheckVersion() {
-    if (parseInt(Services.appinfo.version.split(".")[0]) > 115) {
+    if (parseInt(Services.appinfo.version.split(".")[0]) != 115) {
         _ucUtils.showNotification({
             label : `This version of Beautyfox requires Firefox 115.`,
             type : "bf-notification",
             priority: "critical",
             buttons: []
         })
+    } else if (AppConstants.platform != "win") {
+        _ucUtils.showNotification({
+            label : `This version of Beautyfox requires Windows.`,
+            type : "bf-notification",
+            priority: "critical",
+            buttons: []
+        })
+    } else if (window.matchMedia("(-moz-platform: windows-win10)")) { // Ensure dcomp is set correctly
+        let NCP = window.matchMedia("(-moz-ev-native-controls-patch)").matches;
+        if (pref("gfx.webrender.dcomp-win.enabled").tryGet.bool() == NCP) {
+			pref("gfx.webrender.dcomp-win.enabled").set.bool(!NCP);
+			_ucUtils.restart(false);
+		}
     }
 	// Fetch remote version with timestamp to prevent caching
 	fetch("https://api.github.com/repos/angelbruni/Beautyfox/releases?page=1&per_page=1", {cache: "reload", headers: {"X-GitHub-Api-Version": "2022-11-28", "Accept": "application/vnd.github+json",}})
